efc8870f8ec8745e0179f5a35af559f3cf38f1d8
Luiz Eduardo Sol <luizedusol@gmail.com>
2017-10-02 12:50:33 -0300

    Implements the hello-world of flask on the server module

--- uStockMarket/server.py
+++ uStockMarket/server.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python
+#!flask/bin/python
 # -*- coding: utf-8 -*-
+
+from flask import Flask
 
 from u_stock_market import StockExchange
 
-
+app = Flask(__name__)
 sx = StockExchange()
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
